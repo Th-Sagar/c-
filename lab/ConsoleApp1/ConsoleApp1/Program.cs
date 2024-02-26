223ec88549cb269e2f0923acd3fcb6596215f223
Th-Sagar <sagarthapa98122@gmail.com>
2024-02-26 23:26:41 +0545

    lab 1

---
+++ lab/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Employee
+{
+    public string firstname;
+    public string lastname;
+    public int salary;
+
+    public Employee() { }
+
+    public Employee(string firstname, string lastname, int salary) {
+
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.salary = salary;
+    
+    }
+
+    public void Show()
+    {
+        Console.WriteLine("Frist Name :" + firstname);
+        Console.WriteLine("Last Name :" + lastname);
+        Console.WriteLine("Salary :" +  salary);
+    }
+    public void IncrementSalary()
+    {
+        this.salary = this.salary + 5000;
+    }
+}
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee point = new Employee();
+            point.firstname = "Ram";
+            point.lastname = "Bahadur";
+            point.salary = 20000;
+            point.Show();
+            point.IncrementSalary();
+            point.firstname = "Hari";
+            point.Show();
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ lab/lab_1/Interface/Interface/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public interface IAdm
+{
+    void Manage();
+}
+
+public interface IT
+{
+    void Code();
+}
+
+public class Admstaff : IAdm
+{
+    public void Manage()
+    {
+        Console.WriteLine("Managing administrative tasks.");
+    }
+}
+
+public class ITstaff: IT
+{
+    public void Code()
+    {
+        Console.WriteLine("Write code ");
+    }
+}
+class Interface
+{
+    static void Main(string[] args)
+    {
+        Admstaff adms = new Admstaff();
+        ITstaff its = new ITstaff();
+
+        Console.WriteLine("Administrative staff:");
+        adms.Manage();
+
+
+        Console.WriteLine("Technical staff:");
+        its.Code();
+
+
+        Console.ReadKey();
+
+    }
+}
