1345f8453cbcd6d80623c350d78db9cb1ba34e99
Th-Sagar <sagarthapa98122@gmail.com>
2024-07-17 22:07:12 +0545

    feat:

---
+++ Sagar_Thapa/Razor/Razor/Controllers/UserMgmtController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Razor.Controllers
+{
+    public class UserMgmtController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [ViewData]
+        public string Title { get; set; }
+        public IActionResult Register()
+        {
+            //ViewData["Greeting"] = "Hello";
+            //Title="this is the register page";
+            /* ViewData["Address"] = new Address()
+             {
+                 Name = "steve",
+                 Street = "123 Main St",
+                 City = "Anytown",
+                 State = "TX",
+                 PostalCode = "12345"
+             };
+            */
+
+
+            /*
+             ViewBag.Greeting = "Hello";
+            ViewBag.Address = new Address()
+            {
+                Name = "steve",
+                Street = "123 Main St",
+                City = "Anytown",
+                State = "TX",
+                PostalCode = "12345"
+            };
+
+            */
+
+            return View();
+        }
+    }
+}
---
+++ Sagar_Thapa/Unit-1/Unit-1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DelegateExample
+{
+    public delegate void addnum(int a, int b);
+
+
+    class SingleDelegata
+    {
+        public void sum(int a, int b)
+        {
+
+            Console.WriteLine(a + b);
+
+        }
+    }
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            SingleDelegata obj = new SingleDelegata();
+            addnum sum1= new addnum(obj.sum);
+
+            sum1(10, 2);
+
+        }
+    }
+}
+ 
